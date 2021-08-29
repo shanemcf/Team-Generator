7631be47740a82552493ec8eaba4e77c43030b6f
shanemcf <shanemcfadden95@yahoo.com>
2021-08-29 10:45:47 -0500

    ran node to add an index file

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-// const util = require("util");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
